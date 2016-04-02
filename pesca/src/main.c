fd2e9503dbe25bdac57b36f2377914f4750c8984
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-04-02 23:25:21 +0100

    method to lanch the process of each entity capitao, barco and cardume

--- pesca/src/main.c
+++ pesca/src/main.c
@@ -23,13 +23,15 @@ int main (int argc, char *argv[])
 	pid_t *pid_filhos;
 	processa_parametros (argc, argv);
 	/* criar memória dinâmica para pid_filhos */
-	//INSERIR CÓDIGO
+	pid_filhos = (pid_t *) malloc(sizeof(pid_t) * num_barcos+num_cardumes+1);
+
 	iniciar_mundo ();
 	iniciar_barcos ();
 	iniciar_cardumes ();
 	abrir_log_mundo ();
 	criar_processos (pid_filhos);
 	instalar_rotina_atendimento_sinal ();
+
 	printf ("Simulação arrancou\n");
 	esperar_processos (pid_filhos);
 	destruir_mundo ();
@@ -37,30 +39,49 @@ int main (int argc, char *argv[])
 	destruir_cardumes ();
 	fechar_log_mundo ();
 	/* libertar memória dinâmica de pid_filhos */
-	//INSERIR CÓDIGO
+	free(pid_filhos);
 	return 0;
 }
 
-void criar_processos (pid_t *pid_filhos)
-{
-	int i;
-	/* lançar os processos barcos */
-	//INSERIR CÓDIGO
-	/* lançar os processos cardumes */
-	//INSERIR CÓDIGO
-	/* lançar o processo capitão */
-	//INSERIR CÓDIGO
+void forkCalls(int quant, pid_t *pid_filhos, char *name){
+	int i, pid_filho;
+	for (i = 0; i < quant; i++){
+		pid_filho = fork();
+		if(pid_filho == -1)
+			exit(-1);
+		else{
+			if (pid_filho == 0)
+				exit(0);
+			else{
+				pid_filhos[i] = pid_filho;
+				quant == 1 ? 
+					printf("Lancou o processo filho (PID=%d) para o %s\n", pid_filhos[i], name):
+					printf("Lancou o processo filho (PID=%d) para o %s %d\n", pid_filhos[i], name, i);				
+			}
+		}
+	}
+}
+void criar_processos (pid_t *pid_filhos){
+	/*lancar processos para os barcos*/
+	forkCalls(num_barcos, pid_filhos, "barco");
+	/*lancar processos para os cardumes*/
+	forkCalls(num_cardumes, pid_filhos, "cardume");
+	/*lancar processos para o capitao*/
+	forkCalls(1, pid_filhos, "capitao");
 }
 
 void esperar_processos (const pid_t *pid_filhos)
 {
-	int i;
+	int i, status;
 	int terminados = 0;
-	//INSERIR CÓDIGO
+	for (i=0; i<num_barcos+num_cardumes+1; i++){
+		waitpid(pid_filhos[i], &status, 0);
+		printf("Processo filho para o cardume %d, %s\n", i, tratar_saida_retorno_filho(status));
+
+	}
 }
 
-bool tratar_saida_retorno_filho (int status)
-{
+bool tratar_saida_retorno_filho (int status){
 	bool terminou = false;
 	if (WIFEXITED (status)) {
 		printf (", terminou com status = %d\n", WEXITSTATUS(status));
