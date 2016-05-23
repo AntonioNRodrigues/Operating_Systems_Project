7537861c9ca0740c636487319d816581adf62734
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-05-23 19:16:45 +0100

    delete files

--- pesca/src/logplayer.d
+++ pesca/src/logplayer.d
@@ -1,1 +1,3 @@
-obj/logplayer.o: src/logplayer.c
+obj/logplayer.o: src/logplayer.c include/mundo.h include/capitao.h \
+ include/parametros.h include/posicao.h include/fifo.h include/barco.h \
+ include/cardume.h
